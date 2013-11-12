1865b24804da0b7f83e82d19c27909858131efc3
shuwangx <shux.wang@intel.com>
2013-11-12 14:33:51 +0800

    pull all aplogs

--- scripts/dumplog.sh
+++ scripts/dumplog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#set -x
+set -x
 
 if [ "$DEV_SERIAL" == "" ]; then
     DEV_SERIAL=$1
@@ -20,7 +20,7 @@ if [ "$1" == "clean" -o "$2" == 'clean' ]; then
 fi
 
 
-TIMESTAMP=`date +"%F_%T"`
+TIMESTAMP=`date +"%y%m%d_%H%M%S"`
 HASHTAG=`echo $TIMESTAMP | md5sum`
 HASHTAG=${HASHTAG:0:6}-$DEV_SERIAL-$TIMESTAMP
 
@@ -34,9 +34,13 @@ adb -s $DEV_SERIAL shell getprop > ${LOGDIR}/getprop
 
 adb -s $DEV_SERIAL logcat -v time -d > $LOGDIR/logcat
 adb -s $DEV_SERIAL shell dmesg > $LOGDIR/dmesg
-
-adb -s $DEV_SERIAL pull /logs/aplog $LOGDIR
 adb -s $DEV_SERIAL pull /logs/history_event $LOGDIR
+
+APLOGS=`adb shell ls /logs/aplog* | sed "s/\r//g"`
+for apfile in $APLOGS
+do
+    adb -s $DEV_SERIAL pull $apfile $LOGDIR
+done
 
 if [ "$1" == "tar" -o "$2" == 'tar' ]; then
     tar czvf ~/logs/${HASHTAG}.tar.gz -C ~/logs/ ${HASHTAG}
