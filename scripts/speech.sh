89440679452cbf10d57a700792c17175da0b324d
cblhxx <cblhxx@hotmail.com>
2014-02-10 01:58:26 +0800

    update speech.sh, read from stdin.

--- scripts/speech.sh
+++ scripts/speech.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
-#set -x
 say_us() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?tl=en&q=$*"; }
 say_uk() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.co.uk/translate_tts?tl=en&q=$*"; }
+say_al() { say_us $*; say_uk $*; }
+
+SAY=say_us
+
 if [ "$1" == "_uk" ]; then
     shift
-    say_uk $* > /dev/null 2>&1
-    echo $*
-    exit 0
+    SAY=say_uk
 fi
 
 if [ "$1" == "_al" ]; then
     shift
-    say_us $* > /dev/null 2>&1
-    say_uk $* > /dev/null 2>&1
+    SAY=say_al
+fi
+
+
+if [ "$#" != 0 ]; then
     echo $*
+    sleep 1
+    $SAY $* > /dev/null 2>&1
     exit 0
 fi
 
-say_us $* > /dev/null 2>&1
-echo $*
+IDX=1
+while read WORD;
+do
+    echo $IDX $WORD
+    IDX=`expr $IDX + 1`
+    sleep 1
+    $SAY $WORD > /dev/null 2>&1
+done
